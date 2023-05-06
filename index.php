dcebe53e781995d314eb1adf8331285eda7556e5
lanceskylerr <caolb@students.nu-baliwag.edu.ph>
2023-05-07 00:04:51 +0800

    fixed login css

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
-$nam = array("Sam Espino", "Shae Smith", "Third Lee");
-$ema = array("sam@gmail.com", "shae@gmail.com", "third@gmail.com");
-$pas = array("secret1", "secret2", "secret3");
+$names = array("Sam Espino", "Shae Smith", "Third Lee");
+$emails = array("sam@gmail.com", "shae@gmail.com", "third@gmail.com");
+$passwords = array("secret1", "secret2", "secret3");
 
 
 if (isset($_POST['login'])) {
     $emailInput = $_POST["email"];
     $passwordInput = $_POST["password"];
 
-    foreach ($ema as $key => $email) {
-        if ($email == $emailInput && $pas[$key] == $passwordInput) {
+    foreach ($emails as $key => $email) {
+        if ($email == $emailInput && $passwords[$key] == $passwordInput) {
             echo "logged in";
-            $_SESSION["name"] = $nam[$key];
+            $_SESSION["name"] = $names[$key];
             header("Location: profile.php");
             die();
         }
@@ -31,7 +31,7 @@ if (isset($_POST['login'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MyyyAppp</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 <form action="" method="POST">
@@ -40,7 +40,6 @@ if (isset($_POST['login'])) {
         <img class="img" src="robot.png" alt="">
         <input class="input" type="text" placeholder="Email Address" id="email" name="email">
         <input class="input" type="password" placeholder="Password" id="password" name="password">
-        
         <div class="container2">
             <button class="button" name="login">Sign In</button>
             <button class="button" onclick="clearInputs()">Clear</button>
